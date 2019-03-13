369ba7c3acc60cc55cb37e4d5c365da8bb2fdedc
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-03-13 12:01:57 -0400

    rest

---
+++ 13-rest/dogs/app/controllers/application_controller.rb
@@ -0,0 +1,36 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    # @dogs = Dog.all
+    erb :dogs
+  end
+
+  get "/dogs/" do
+    @dogs = Dog.all
+    erb :dogs
+  end
+
+  get '/dogs/:id' do
+    @dog = Dog.find(params[:id])
+    erb :show
+  end
+
+  delete "/dogs/:id" do
+    binding.pry
+  end
+
+  get '/search' do
+    @dogs = Dog.search(params[:q])
+    erb :dogs
+  end
+
+
+
+end
