81fadee841e13d680648885b33e316c0df5eeb66
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-03-13 14:11:18 -0400

    rest

--- 13-rest/dogs/app/controllers/application_controller.rb
+++ 13-rest/dogs/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get "/" do
@@ -12,14 +13,63 @@ class ApplicationController < Sinatra::Base
     erb :dogs
   end
 
+  #index
   get "/dogs/" do
     @dogs = Dog.all
     erb :dogs
   end
 
+  #new
+  get "/dogs/new" do
+    erb :new
+  end
+
+  #show
   get '/dogs/:id' do
     @dog = Dog.find(params[:id])
     erb :show
+  end
+
+  #create
+  post '/dogs' do
+    binding.pry
+    @dog = Dog.create(params)
+    redirect to "/dogs/#{ @dog.id }"
+  end
+
+
+  get "/dogs/:id/edit" do
+    @dog = Dog.find(params[:id])
+    erb :edit
+  end
+
+ #  params = {
+ #            "_method"=>"PATCH",
+ #             "name"=>"COSTANZA",
+ #             "tail_length"=>"2",
+ #             "birth_year"=>"1990",
+ #             "fluffiness"=>"0",
+ #             "id"=>"10"
+ #           }
+
+ # params = {
+ #            "_method"=>"PATCH",
+ #             dog: { 
+ #                "name"=>"COSTANZA",
+ #                "tail_length"=>"2",
+ #                "birth_year"=>"1990",
+ #                "fluffiness"=>"0"
+ #              },
+ #             "id"=>"10"
+ #           }
+
+
+  #update
+  patch "/dogs/:id" do
+    # params.delete("_method")
+    @dog = Dog.find(params[:id])
+    @dog.update(params[:dog])
+    redirect to "/dogs/#{ @dog.id }"
   end
 
   delete "/dogs/:id" do
