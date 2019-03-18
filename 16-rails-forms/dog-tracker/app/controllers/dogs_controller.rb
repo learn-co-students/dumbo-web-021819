6a3e4ea7c5158d4ab7f637d526d6a17fe4bcd7d5
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-03-18 12:34:23 -0400

    forms

--- 16-rails-forms/dog-tracker/app/controllers/dogs_controller.rb
+++ 16-rails-forms/dog-tracker/app/controllers/dogs_controller.rb
@@ -1,7 +1,7 @@
 class DogsController < ApplicationController
   # before_action(:get_dog, { only: [:show, :edit] })
   
-  before_action :get_dog, only: [:show, :edit]
+  before_action :get_dog, only: [:show, :edit, :update]
 
   def index
     @dogs = Dog.all
@@ -10,10 +10,28 @@ class DogsController < ApplicationController
   def show
   end
 
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    redirect_to @dog
+  end
+
   def edit
   end
 
+  def update
+    @dog.update(dog_params)
+    redirect_to @dog
+  end
+
   private
+
+  def dog_params
+    params.require(:dog).permit(:name, :tail_length, :color_hexadecimal)
+  end
 
   def get_dog
     @dog = Dog.find(params[:id])
